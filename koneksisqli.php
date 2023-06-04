d83aa9eead7e587b851764efc0f56cfe677de856
Muhammad Rafi Insan Fillah <78022264+Mengz04@users.noreply.github.com>
2023-06-04 13:53:52 +0700

    Update koneksisqli.php

--- koneksisqli.php
+++ koneksisqli.php
@@ -1,7 +1,7 @@
 <?php
 $host       = "localhost";
 $user       = "root";
-$password   = "madiun453";
+$password   = "";
 $database   = "upload_foto";
 $connect    = mysqli_connect($host, $user, $password, $database);
 ?>
